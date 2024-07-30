1d2edd6886b3e9bf15416cf9d4d1b3a35b264903
motola <olutolamoses@gmailcom>
2024-07-30 17:43:07 +0100

    refactoring for db storage

--- schemas.py
+++ schemas.py
@@ -3,10 +3,9 @@ from marshmallow import Schema, fields
 
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema): 
     name = fields.Str()
@@ -14,17 +13,17 @@ class ItemUpdateSchema(Schema):
 
 
 class PlainStoreSchema(Schema): 
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
-    inventory = fields.Str(required=True)
+    inventory = fields.Int(required=True)
 
 
 class ItemSchema(PlainItemSchema):
-    store_id = fields.Int(required=True, dump_only=True)
-    store = fields.Nested(lambda: StoreSchema(), dump_only=True)
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(lambda: PlainStoreSchema(), dump_only=True)
 
 class StoreSchema(PlainStoreSchema):
-   items = fields.List(fields.Nested(lambda: ItemSchema()), dump_only=True)
+   items = fields.List(fields.Nested(lambda: PlainItemSchema()), dump_only=True)
 
 
 
