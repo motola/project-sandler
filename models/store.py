46aa0652491a3a1d4a32c2d69de00a36d7d9b877
motola <olutolamoses@gmailcom>
2024-08-03 23:14:20 +0100

    migration upgrade and deployment on render.com

--- models/store.py
+++ models/store.py
@@ -7,6 +7,7 @@ class StoreModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80),unique=True, nullable=False)
+    description = db.Column(db.String)
     inventory = db.Column(db.String(80),unique=False, nullable=False)
     items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
     tags = db.relationship("TagModel", back_populates="store", lazy="dynamic")
